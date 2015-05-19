9e082323c6d2db32d6ac905975e7086c02b365e0
ethan <e.raymond@comcast.net>
2015-05-19 15:07:24 -0400

    Added a class for JSON Keys.

---
+++ app/src/main/java/com/edr/fingerdodge/json/JSONKeys.java
@@ -0,0 +1,12 @@
+package com.edr.fingerdodge.json;
+
+/**
+ * @author Ethan Raymond
+ */
+public class JSONKeys {
+
+    public static final String KEY_TYPE = "type";
+    public static final String KEY_TIME = "time";
+    public static final String KEY_ACTIVITY_NAME = "activity-name";
+
+}
