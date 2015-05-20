d36f5a4ca779a2ea5b60049656a4822708eb4576
ethan <e.raymond@comcast.net>
2015-05-19 20:08:05 -0400

    Removed JSON library.

--- app/src/main/java/com/edr/fingerdodge/json/JSONKeys.java
+++ app/src/main/java/com/edr/fingerdodge/json/JSONKeys.java
@@ -10,5 +10,6 @@ public class JSONKeys {
     public static final String KEY_ACTIVITY_NAME = "activity-name";
     public static final String KEY_DURATION = "time";
     public static final String KEY_BEAT_HIGH_SCORE = "beat-high-score";
+    public static final String KEY_API = "api";
 
 }
