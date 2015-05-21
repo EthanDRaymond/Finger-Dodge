56d31225809f83399f0dd9234573e82cdb53e9d4
eraymond <e.raymond@comcast.net>
2015-05-21 13:20:59 -0400

    Added JSON library.

--- app/src/main/java/com/edr/fingerdodge/json/JSONKeys.java
+++ app/src/main/java/com/edr/fingerdodge/json/JSONKeys.java
@@ -11,5 +11,10 @@ public class JSONKeys {
     public static final String KEY_DURATION = "time";
     public static final String KEY_BEAT_HIGH_SCORE = "beat-high-score";
     public static final String KEY_API = "api";
+    public static final String KEY_ID = "id";
+    public static final String KEY_USER_ID = "user-id";
+    public static final String KEY_STATISTICS = "statistics";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_CONTENT = "content";
 
 }
